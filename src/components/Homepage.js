d73c9cabd3cd5d9be2883fd828326cb410037fae
Honore Solomon <honoresolomon@hotmail.com>
2023-09-29 12:17:31 -0700

    completed initial homepage component

---
+++ src/components/Homepage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Box, Heading, Paragraph } from 'grommet';
+
+function Homepage() {
+  return (
+    <Box padding='medium'>
+      <Heading level='2'>
+        Welcome To Your Bug Tracker and Project Manager
+      </Heading>
+      <Paragraph></Paragraph>
+    </Box>
+  );
+}
+
+export default Homepage;
